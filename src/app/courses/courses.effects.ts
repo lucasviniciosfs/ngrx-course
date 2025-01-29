9565197fedc1531b10a7b3f7a1d7361e4af92dd0
Lucas <lucasvinicios-fs@hotmail.com>
2025-01-29 19:34:13 +0000

    edit course

--- src/app/courses/courses.effects.ts
+++ src/app/courses/courses.effects.ts
@@ -2,7 +2,7 @@ import { Injectable } from "@angular/core";
 import { Actions, createEffect, ofType } from "@ngrx/effects";
 import { Action } from "@ngrx/store";
 import { CoursesHttpService } from "./services/courses-http.service";
-import { loadAllcoursesAction, loadedAllCoursesAction } from "./courses.actions";
+import { loadAllcoursesAction, loadedAllCoursesAction, saveEditedCourseAction } from "./courses.actions";
 import { concatMap, map, mergeMap } from "rxjs/operators";
 
 @Injectable()
@@ -17,6 +17,15 @@ export class CoursesEffect {
             map((courses) => loadedAllCoursesAction({courses}))
         )
     )
+
+    editCourse$ = createEffect(() => 
+        this.actions$.pipe(
+            ofType(saveEditedCourseAction),
+            concatMap(updateEntity => {
+                return this.coursesService.saveCourse(updateEntity.course.id, updateEntity.course.changes)
+            })
+        )
+    , {dispatch: false})
 
     constructor(private actions$: Actions, private coursesService: CoursesHttpService){}
 }
